25b5d1f6b27ccf6a76aeaa942c40bb358ae32edb
JMahal0 <tajm27@gmail.com>
2023-06-26 16:41:36 +0200

    Centered the map setup instructions

--- src/components/infoContent/MapSetupBasics.tsx
+++ src/components/infoContent/MapSetupBasics.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Dropdown from "../Dropdown";
 
 const MapSetupBasics = () => {
-    const [numPlayers, setNumPlayers] = useState("-");
+    const [numPlayers, setNumPlayers] = useState("");
     const handleNumPlayersChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setNumPlayers(event.target.value);
     }
@@ -16,7 +16,7 @@ const MapSetupBasics = () => {
 
             <p>How many players do you have?
             <select className="ml-2 p-1 rounded-lg" value={numPlayers} onChange={handleNumPlayersChange}>
-                <option value="-">Pick an option</option>
+                <option value="">Pick an option</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
@@ -30,7 +30,7 @@ const MapSetupBasics = () => {
                 <p>
                     Get 17 W, 3 C1, 2 C2, 1 C3, and 2 PS cards and arrange them link this
                 </p>
-                <table className="map-diagram">
+                <table className="map-diagram mx-auto">
                     <tbody>
                     <tr><td>PS</td><td>W</td><td>W</td><td>W</td><td>C2</td></tr>
                     <tr><td>W</td><td>W</td><td>W</td><td>C1</td><td>W</td></tr>
@@ -47,7 +47,7 @@ const MapSetupBasics = () => {
                 <p>
                     Get 21 W, 4 C1, 3 C2, 1 C3, and 3 PS cards and arrange them link this
                 </p>
-                <table className="map-diagram">
+                <table className="map-diagram mx-auto">
                     <tbody>
                     <tr><td className="empty-cell"></td><td>C1</td><td>PS</td><td>W</td><td>C1</td><td className="empty-cell"></td></tr>
                     <tr><td>W</td><td>W</td><td>W</td><td>W</td><td>W</td><td>W</td></tr>
@@ -65,7 +65,7 @@ const MapSetupBasics = () => {
                 <p>
                     Get 24 W, 4 C1, 3 C2, 2 C3, 1 C4, and 4 PS cards and arrange them like this
                 </p>
-                <table className="map-diagram">
+                <table className="map-diagram mx-auto">
                     <tbody>
                     <tr><td className="empty-cell"></td><td>PS</td><td>W</td><td>C3</td><td>W</td><td>PS</td><td className="empty-cell"></td></tr>
                     <tr><td>C1</td><td>W</td><td>W</td><td>C2</td><td>W</td><td>W</td><td>C1</td></tr>
@@ -83,7 +83,7 @@ const MapSetupBasics = () => {
                 <p>
                     Get 28 W, 5 C1, 4 C2, 2 C3, 1 C4, and 5 PS cards and arrange them link this
                 </p>
-                <table className="map-diagram">
+                <table className="map-diagram mx-auto">
                     <tbody>
                     <tr><td className="empty-cell"></td><td>C1</td><td>W</td><td>PS</td><td>W</td><td>C1</td><td className="empty-cell"></td></tr>
                     <tr><td>W</td><td>W</td><td>W</td><td>W</td><td>W</td><td>W</td><td>W</td></tr>
@@ -102,7 +102,7 @@ const MapSetupBasics = () => {
                 <p>
                     Get 32 W, 5 C1, 4 C2, 3 C3, 2 C4, and 6 PS cards and arrange them like this
                 </p>
-                <table className="map-diagram">
+                <table className="map-diagram mx-auto">
                     <tbody>
                     <tr><td className="empty-cell"></td><td className="empty-cell"></td><td>C1</td><td>W</td><td>W</td><td>PS</td><td className="empty-cell"></td><td className="empty-cell"></td></tr>
                     <tr><td className="empty-cell"></td><td>PS</td><td>W</td><td>W</td><td>C2</td><td>W</td><td>C1</td><td className="empty-cell"></td></tr>
@@ -116,20 +116,22 @@ const MapSetupBasics = () => {
                 </table>
             </>
             }
-            <li>
-                Put the W cards and city cards down first
-            </li>
-            <li>
-                Put 1 follower token on all the size-1 cities and 2 follower on all the other cities
-            </li>
-            <li>
-                Then take turns placing your spawn card on the remaining spaces in the map
-                {/* // FAQ How do we decide who goes first? Everyone rolls the dice and the person that rolls the highest goes first. If there is tie, then the youngest in the tie goes first. Then go clockwise for the next person until everyone has a turn. */}
-            </li>
-            <li>
-                Finally, put your Avatar piece on your spawn card on the tile that has your element icon
-                {/* //add an image of the spawn tile (God of Air's starting tile) */}
-            </li>
+            {numPlayers !== "" && <>
+                <li>
+                    Put the W cards and city cards down first
+                </li>
+                <li>
+                    Put 1 follower token on all the size-1 cities and 2 follower on all the other cities
+                </li>
+                <li>
+                    Then take turns placing your spawn card on the remaining spaces in the map
+                    {/* // FAQ How do we decide who goes first? Everyone rolls the dice and the person that rolls the highest goes first. If there is tie, then the youngest in the tie goes first. Then go clockwise for the next person until everyone has a turn. */}
+                </li>
+                <li>
+                    Finally, put your Avatar piece on your spawn card on the tile that has your element icon
+                    {/* //add an image of the spawn tile (God of Air's starting tile) */}
+                </li>
+            </> }
             </ul>
         </>}/>
     );
